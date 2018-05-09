91b7c8dfac94a5503027599290763aed3f7e45ff
Jed <lejosnej@ainfosec.com>
2018-05-09 10:22:56 -0400

    xenmgr: do not start uivm/ndvm if they don't exist
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- xenmgr/Main.hs
+++ xenmgr/Main.hs
@@ -295,7 +295,14 @@ options =
     , Option "h" ["help"] (NoArg Help) "print usage information" ]
 
 useServiceVm :: Uuid -> Rpc Bool
-useServiceVm uuid = return True
+useServiceVm uuid = do
+  name <- getVmName uuid
+  case name of
+    "uivm" ->
+        return $ liftIO $ doesDirectoryExist "/storage/uivm"
+    "Network" ->
+        return $ liftIO $ doesDirectoryExist "/storage/ndvm"
+    _ -> return True
 
 initServiceVms :: XM [Uuid]
 initServiceVms = liftRpc $ do
