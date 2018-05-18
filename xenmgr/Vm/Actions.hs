c40fc5c2bcc72cff71264765f1c8cb9fd4fe6fe1
Jason Andryuk <jandryuk@gmail.com>
2018-05-18 10:47:44 -0400

    xenmgr: Select ndvm pv/hvm mode at runtime
    Allow choosing the ndvm VM type by setting dbd node /xenmgr/ndvm-mode to
    either "pv" or "hvm".  It defaults to "pv" for now.  A reboot is
    required to take effect.
    
    OXT-1327
    
    Signed-off-by: Jason Andryuk <jandryuk@gmail.com>

--- xenmgr/Vm/Actions.hs
+++ xenmgr/Vm/Actions.hs
@@ -334,7 +334,8 @@ configureServiceVm tag =
 -- remove service vms which are not in active templates from db
 trashUnusedServiceVms :: Rpc ()
 trashUnusedServiceVms =
-    do tags <- liftIO enumServiceVmTags
+    do ndvm_mode <- getNdvmMode
+       tags <- liftIO $ enumServiceVmTags ndvm_mode
        mapM_ (trash tags) =<< getVms
        where
          trash active uuid =
